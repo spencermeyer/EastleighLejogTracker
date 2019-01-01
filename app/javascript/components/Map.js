0a409bec12e1800ebcd64ee99b35c780468560ed
spencermeyer <spencer_meyer@hotmail.com>
2019-01-01 15:25:49 +0000

    managed to put a marker on the map

--- app/javascript/components/Map.js
+++ app/javascript/components/Map.js
@@ -1,20 +1,33 @@
 import React from "react"
 import PropTypes from "prop-types"
 import GoogleMapReact from 'google-map-react';
+import $ from 'jquery'
+import Marker from 'components/Marker'
 
 class Map extends React.Component {
   static defaultProps = {
     center: {
-      lat: 52.54,
-      lng: -1.77
+      // lat: 52.54,
+      lat: 51.54,
+      lng: -5.77
+      // lng: -1.77
     },
     zoom: 7
   };
 
   componentDidMount() {
     console.log('hello from CDM');
+    $.ajax({
+      url: '/end_to_end_example_main_route.gpx',
+      dataType: 'xml',
+      success: function(data){
+        console.log('we got' + data.getElementsByTagName('trkpt').length + 'points');
+        console.log('first lat is ' + data.getElementsByTagName('trkpt')[1].getAttribute('lat'));
+        console.log('first lng is ' + data.getElementsByTagName('trkpt')[1].getAttribute('lon'));
+        // debugger;
+      }
+    });
   }  
-
 
   render () {
     return (
@@ -23,8 +36,9 @@ class Map extends React.Component {
           bootstrapURLKeys={{ key: 'AIzaSyD3nqUGFkDrCCDA8os-XGLGCMRxRCYW0tU' }}
           defaultCenter={this.props.center}
           defaultZoom={this.props.zoom}
+          yesIWantToUseGoogleMapApiInternals
         >
-
+          <Marker lat = {50.06605} lng = {-5.71292}/> 
         </GoogleMapReact>
       </div>
     );
@@ -32,3 +46,7 @@ class Map extends React.Component {
 }
 
 export default Map
+
+
+
+
---
+++ app/javascript/components/Marker.js
@@ -0,0 +1,13 @@
+import React from "react"
+import PropTypes from "prop-types"
+class Marker extends React.Component {
+  render () {
+    return (
+      <div>
+        X
+      </div>
+    );
+  }
+}
+
+export default Marker
