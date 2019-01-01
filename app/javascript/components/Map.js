5cbe7c8bd0fd38bf7fa739ace43ffd1ffc1155df
spencermeyer <spencer_meyer@hotmail.com>
2019-01-01 11:23:05 +0000

    got a google map working

--- app/javascript/components/Map.js
+++ app/javascript/components/Map.js
@@ -1,9 +1,32 @@
 import React from "react"
 import PropTypes from "prop-types"
+import GoogleMapReact from 'google-map-react';
+
 class Map extends React.Component {
+  static defaultProps = {
+    center: {
+      lat: 52.54,
+      lng: -1.77
+    },
+    zoom: 7
+  };
+
+  componentDidMount() {
+    console.log('hello from CDM');
+  }  
+
+
   render () {
     return (
-      <h4>here is js map</h4>
+      <div style={{ height: '100vh', width: '100%' }}>
+        <GoogleMapReact
+          bootstrapURLKeys={{ key: 'AIzaSyD3nqUGFkDrCCDA8os-XGLGCMRxRCYW0tU' }}
+          defaultCenter={this.props.center}
+          defaultZoom={this.props.zoom}
+        >
+
+        </GoogleMapReact>
+      </div>
     );
   }
 }
