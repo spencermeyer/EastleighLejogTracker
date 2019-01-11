bceea9bc33e99610c0e74eed54df2c1c202e697a
spencermeyer <spencer_meyer@hotmail.com>
2019-01-11 11:05:07 +0000

    tidied some views and annotated models

--- app/models/run.rb
+++ app/models/run.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: runs
+#
+#  id            :bigint(8)        not null, primary key
+#  strava_run_id :integer
+#  distance      :integer
+#  date          :datetime
+#  user_id       :integer
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#
+
 class Run < ApplicationRecord
   belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,35 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id                            :bigint(8)        not null, primary key
+#  email                         :string           default(""), not null
+#  encrypted_password            :string           default(""), not null
+#  reset_password_token          :string
+#  reset_password_sent_at        :datetime
+#  remember_created_at           :datetime
+#  sign_in_count                 :integer          default(0), not null
+#  current_sign_in_at            :datetime
+#  last_sign_in_at               :datetime
+#  current_sign_in_ip            :inet
+#  last_sign_in_ip               :inet
+#  created_at                    :datetime         not null
+#  updated_at                    :datetime         not null
+#  strava_id                     :integer
+#  first_name                    :text
+#  last_name                     :text
+#  screen_name                   :text
+#  strava_access_token           :text
+#  strava_refresh_token          :text
+#  strava_access_token_expiry_in :integer
+#  strava_access_token_expiry_at :integer
+#  strava_athlete_auth_id        :integer
+#  strava_athlete_auth_username  :text
+#  strava_athlete_auth_firstname :text
+#  strava_athlete_auth_lastname  :text
+#  strava_athlete_auth_email     :text
+#
+
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
