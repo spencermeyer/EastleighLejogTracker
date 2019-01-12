6562b17f26e061173242a4786c4dc0e043632932
spencermeyer <spencer_meyer@hotmail.com>
2019-01-12 20:24:44 +0000

    configed the callback url

--- app/helpers/map_helper.rb
+++ app/helpers/map_helper.rb
@@ -1,5 +1,13 @@
 module MapHelper
   def strava_authorize_request_path
-    'https://www.strava.com/oauth/authorize' + '?client_id=' + ENV['STRAVA_CLIENT_ID'] + '&response_type=code' + '&redirect_uri=https://fa5fda02.ngrok.io/strava_auth&scope=activity:read_all&approval_prompt=force'
+    "https://www.strava.com/oauth/authorize' + '?client_id=' + ENV['STRAVA_CLIENT_ID'] + '&response_type=code' + '&redirect_uri=#{strava_callback_url}/strava_auth&scope=activity:read_all&approval_prompt=force"
+  end
+
+  def strava_callback_url
+    if Rails.env.production?
+      'https://eastleigh-lejog-tracker.herokuapp.com'
+    else
+      'https://e6102eac.ngrok.io'
+    end
   end
 end
