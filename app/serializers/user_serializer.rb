376314506a9696d9ce1187c053f2abc697ea9352
spencermeyer <spencer_meyer@hotmail.com>
2019-01-23 20:47:20 +0000

    converting displays to miles

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -2,6 +2,6 @@ class UserSerializer < ActiveModel::Serializer
   attributes :screen_name, :total_miles
 
   def total_miles
-    self.object.total_miles
+    self.object.total_miles/1609
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -10,14 +10,14 @@ user1 = User.create(email: 'elvis@somewhere.com', strava_id: 1234, first_name: '
 user2 = User.create(email: 'micket@mouse.com', strava_id: 1235, first_name: 'mickey', last_name: 'mouse', screen_name: 'MickeyMouse', admin: false, password: 'password')
 user3 = User.create(email: 'minnie@mouse.com', strava_id: 1236, first_name: 'minnie', last_name: 'mouse', screen_name: 'MinnieMouse', admin: false, password: 'password')
 
-run1 = Run.create(strava_run_id: 100000000, distance: 133, user_id: user1.id)
-run1 = Run.create(strava_run_id: 100000001, distance: 144, user_id: user2.id)
-run1 = Run.create(strava_run_id: 100000002, distance: 155, user_id: user3.id)
-run1 = Run.create(strava_run_id: 100000003, distance: 166, user_id: user1.id)
-run1 = Run.create(strava_run_id: 100000004, distance: 177, user_id: user2.id)
-run1 = Run.create(strava_run_id: 100000005, distance: 177, user_id: user3.id)
-run1 = Run.create(strava_run_id: 100000006, distance: 177, user_id: user1.id)
-run1 = Run.create(strava_run_id: 100000007, distance: 177, user_id: user2.id)
+run1 = Run.create(strava_run_id: 100000000, distance: 1609*3, user_id: user1.id)
+run2 = Run.create(strava_run_id: 100000001, distance: 1609*4, user_id: user2.id)
+run3 = Run.create(strava_run_id: 100000002, distance: 1609*9, user_id: user3.id)
+run4 = Run.create(strava_run_id: 100000003, distance: 1609*6, user_id: user1.id)
+run5 = Run.create(strava_run_id: 100000004, distance: 1609*9, user_id: user2.id)
+run6 = Run.create(strava_run_id: 100000005, distance: 1609*7, user_id: user3.id)
+run7 = Run.create(strava_run_id: 100000006, distance: 1609*8, user_id: user1.id)
+run8 = Run.create(strava_run_id: 100000007, distance: 1609*7, user_id: user2.id)
 
 
 
