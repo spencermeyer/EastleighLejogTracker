9255079e74d41d296c6db6a313f99db0068b5f74
spencermeyer <spencer_meyer@hotmail.com>
2019-02-10 13:44:23 +0000

    trying to get initial capistrano wo work

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,7 +4,7 @@ lock "3.11.0"
 
 server '209.97.129.237', roles: [:web, :app, :db], primary: true
 
-set :repo_url, "ggit@github.com:spencermeyer/EastleighLejogTracker.git"
+set :repo_url, "git@github.com:spencermeyer/EastleighLejogTracker.git"
 
 set :application, "EastleighLejogTracker"
 set :user, 'deploy'
@@ -24,7 +24,7 @@ set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
 set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
 set :puma_access_log, "#{release_path}/log/puma.error.log"
 set :puma_error_log,  "#{release_path}/log/puma.access.log"
-set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa.pub) }
+set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa) }
 set :puma_preload_app, true
 set :puma_worker_timeout, nil
 set :puma_init_active_record, true  # Change to false when not using ActiveRecord
