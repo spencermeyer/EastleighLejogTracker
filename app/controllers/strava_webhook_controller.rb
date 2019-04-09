39a755067d6154f01d3b702f1ed2c500e73d9c53
spencermeyer <spencer_meyer@hotmail.com>
2019-04-09 16:56:35 +0100

    correcting error in the webhook controller was trying to find user by id not by strava id

--- app/controllers/strava_webhook_controller.rb
+++ app/controllers/strava_webhook_controller.rb
@@ -7,8 +7,9 @@ class StravaWebhookController < ApplicationController
     end
 
     if params['aspect_type'] == 'create' && params['object_type'] == 'activity'
-      user = User.find(params['owner_id'])
+      user = User.find_by_strava_id(params['owner_id'])
       run = Run.create(user_id: user.id, strava_run_id: params['object_id'])
+      # TODO put this in a resque block
       CollectRunDataJob.perform(run.id)
     end
 
@@ -36,4 +37,11 @@ end
 # Processing by StravaWebhookController#webhook as HTML
 # I, [2019-04-07T19:14:08.990556 #13092]  INFO -- : [7c8a14a1-2cfa-464e-b721-e4e8b6c37fc1]   Parameters: {"aspect_type"=>"create", "event_time"=>1554664448, "object_id"=>2273162956, "object_type"=>"activity", "owner_id"=>21382538, "subscription_id"=>135744, "updates"=>{}, "strava_webhook"=>{"aspect_type"=>"create", "event_time"=>1554664448, "object_id"=>2273162956, "object_type"=>"activity", "owner_id"=>21382538, "subscription_id"=>135744, "updates"=>{}}}
 # I, [2019-04-07T19:14:08.990876 #13092]  INFO -- : [7c8a14a1-2cfa-464e-b721-e4e8b6c37fc1] AwoogaX #<StringIO:0x00000003d4d340>
+
+
+
+
+# Parameters: {"aspect_type"=>"create", "event_time"=>1554825107, "object_id"=>2277195168, "object_type"=>"activity", "owner_id"=>21382538, "subscription_id"=>135744, "updates"=>{}, "strava_webhook"=>{"aspect_type"=>"create", "event_time"=>1554825107, "object_id"=>2277195168, "object_type"=>"activity", "owner_id"=>21382538, "subscription_id"=>135744, "updates"=>{}}}
+
+
 
