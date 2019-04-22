ebcd443b3b6b683d45ece0df008f7344910f1119
spencermeyer <spencer_meyer@hotmail.com>
2019-04-22 11:59:53 +0100

    stub call to strava

--- spec/controllers/strava_webhook_controller_spec.rb
+++ spec/controllers/strava_webhook_controller_spec.rb
@@ -10,6 +10,8 @@ RSpec.describe StravaWebhookController, type: :controller do
 
     params = {"aspect_type"=>"create", "event_time"=>1554825872, "object_id"=>2277221429, "object_type"=>"activity", "owner_id"=>21382538, "subscription_id"=>135744, "updates"=>{}, "strava_webhook"=>{"aspect_type"=>"create", "event_time"=>1554825872, "object_id"=>2277221429, "object_type"=>"activity", "owner_id"=>21382538, "subscription_id"=>135744, "updates"=>{}}}
 
+    allow(CollectRunDataJob).to receive(:perform).and_return({something: 'testing'})
+
     expect{
       post :webhook, :params => params
     }.to change { user.runs.count }.by(1)
