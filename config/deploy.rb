ca19049bfc0f610b86f91e8b033031f93cf52ab9
spencer meyer <spencer_meyer@hotmail.com>
2021-06-12 14:49:04 +0100

    Alter origin for deploys

--- config/deploy.rb
+++ config/deploy.rb
@@ -67,7 +67,7 @@ namespace :deploy do
   desc "Make sure local git is in sync with remote."
   task :check_revision do
     on roles(:app) do
-      unless `git rev-parse HEAD` == `git rev-parse github/master`
+      unless `git rev-parse HEAD` == `git rev-parse origin/master`
       # unless `git rev-parse HEAD` == `git rev-parse bitbucket/master`
         puts "WARNING: HEAD is not the same as origin/master"
         # puts "WARNING: HEAD is not the same as bitbucket/master"
